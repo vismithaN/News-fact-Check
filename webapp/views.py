fed1591dc5081e603967972d3a667fa3e31c4562
Vismitha N <vismitha944@gmail.com>
2024-05-01 14:40:03 -0400

    Beautify

--- webapp/views.py
+++ webapp/views.py
@@ -2,9 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    input_text = request.GET.get('input_text', '')
-
-    # Example processing (you can modify this logic as needed)
-    output_text = f"You entered: {input_text}"
+    if 'input_text' in request.GET:
+        input_text = request.GET['input_text']
+        output_text = input_text
+    else:
+        output_text = None  # No output if nothing has been submitted
 
     return render(request, 'index.html', {'output_text': output_text})
